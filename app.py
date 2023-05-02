cb363f3d430da4a76b215c1f716f38b8d3989a92
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2023-05-02 12:03:08 +0300

    add a function that opens the saved_meals.txt and allows the user to edit what is inside the meal list

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tkinter
 import customtkinter
+import os
 
 customtkinter.set_appearance_mode('light')
 customtkinter.set_default_color_theme('green')
@@ -10,33 +11,43 @@ class GUI(customtkinter.CTk):
         self.title('PickMEal')
         self.geometry('1280x720')
         self.config(pady=40)
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure((0), weight=1)
 
         #create the main label
         self.greeting = customtkinter.CTkLabel(self, text='Welcome to PickMEal', 
                                                anchor='center', font=('Facit', 65),
                                                text_color='SpringGreen2')
-        self.greeting.grid(row=0, column=0, padx=40, pady=5)
+        self.greeting.grid(row=0, column=0, columnspan=1, padx=40, pady=5)
 
         #create a label with a brief description
         self.description = customtkinter.CTkLabel(self, 
                                                    text='Your custom meal picker!',
                                                    font=('Facit', 20))
-        self.description.grid(row=1, column=0)
+        self.description.grid(row=1, column=0, columnspan=1)
 
         #create an entry for new input
         self.new_meals = customtkinter.CTkEntry(self, width=500, placeholder_text='Enter one or multiple meals, separated by a comma')
-        self.new_meals.grid(row=2, column=0, pady=20)
+        self.new_meals.grid(row=2, column=0, columnspan=1, pady=20)
 
         #create a button to store the entry input
-        self.btn_save = customtkinter.CTkButton(self, text='Save', command=self.save_meals)
-        self.btn_save.grid(row=3,column=0, pady=10)
+        self.save_btn = customtkinter.CTkButton(self, text='Save', command=self.save_meals)
+        self.save_btn.grid(row=3,column=0, padx=20, pady=10)
 
+        #create a button to delete an existing meal from the saved_meals.txt
+        self.del_btn = customtkinter.CTkButton(self, text='Delete', command=self.del_meals)
+        self.del_btn.grid(row=3, column=1, padx=20, pady=10)
+    
     def save_meals(self):
         user_input = self.new_meals.get()
         if len(user_input) > 0:
             with open('saved_meals.txt', 'a') as file:
                 file.write(f'{user_input}\n')
+    
+    def del_meals(self):
+        try:
+            os.startfile('saved_meals.txt')
+        except FileNotFoundError:
+            pass
 
 
 
