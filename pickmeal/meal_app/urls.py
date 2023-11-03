fd46e232fca6a991f5cc58ee72158544d795a157
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2023-11-03 14:43:02 +0200

    change api request, display results based off user search

--- pickmeal/meal_app/urls.py
+++ pickmeal/meal_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('register', views.register, name='register'),
     path('login', views.log_in, name='login'),
-    path('logout', views.log_out, name='logout')
+    path('logout', views.log_out, name='logout'),
+    path('recipe/<int:recipe_id>', views.recipe, name='recipe'),
+    path('results', views.results, name='recipe_results')
 ]
--- pickmeal/meal_app/views.py
+++ pickmeal/meal_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
-from django.db import IntegrityError
 import requests
 import json
 import os
@@ -15,39 +14,61 @@ def getRecipes(ingredients):
     api_key = settings.SPOONACULAR_API_KEY
 
     params = {
-        'apiKey': api_key,
-        'ingredients': ingredients,
-        'number': 5
+    'apiKey': api_key,
+    'ingredients': ingredients, 
+    'ranking': 1,
+    'number': 3
     }
+
+
+
     try:
         response = requests.get(url, params=params)
-        return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Request error: {e}")
         return None
+    
+    data = response.json()
+    recipe_ids = ''
+    results = {}
+    for recipe in data:
+        id = recipe['id']
+        recipe_ids += f'{id},'
+        title = recipe['title'].title()
+        image = recipe['image']
+        num_used_ingredients = recipe['usedIngredientCount']
+        num_missing_ingredients = recipe['missedIngredientCount']
+        missing_ingredients = [j['name'].title() for j in recipe['missedIngredients']]
+        used_ingredients = [j['name'].title() for j in recipe['usedIngredients']]
+        results[title] = {
+            'id': id,
+            'title': title,
+            'image': image,
+            'num_used_ings': num_used_ingredients,
+            'num_missing_ings': num_missing_ingredients,
+            'missing_ings': missing_ingredients,
+            'used_ings': used_ingredients
+        }
+    return results
+
+
+def recipeInformation(recipe_id):
+    url = f'https://api.spoonacular.com/recipes/{recipe_id}/information'
+    api_key = settings.SPOONACULAR_API_KEY
+    params = {
+        'apiKey': api_key
+    }
+    try:
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            return json.loads(response.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
+        return None
+    
 
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
-        data = request.POST
-        ingredients = data.get('recipe-search')
-        # recipes = getRecipes(ingredients)
-        # if not recipes:
-        #     # handle error
-        #     ...
-        json_file_path = os.path.join(settings.BASE_DIR, 'meal_app/static', 'working_data.json')
-
-        with open(json_file_path, 'r') as file:
-            recipes = json.load(file)
-
-        results = [{'title': item['title'], 
-                    'image': item['image'], 
-                    'missing_ings': [ingredient['name'] for ingredient in item['missedIngredients']]
-                    } for item in recipes]
-        
-        return render(request, 'results.html', context={
-            'results': results
-            })
     return render(request, 'index.html')
 
 
@@ -98,3 +119,27 @@ def register(request):
     return render(request, 'register.html', {
         'form': form
     })
+
+def results(request):
+    if request.method == 'POST':
+        data = request.POST
+        ingredients = data.get('recipe-search')
+        # recipes = getRecipes(ingredients)
+        recipes= {'Goat Cheese Pesto Pizza': {'id': 644953, 'title': 'Goat Cheese Pesto Pizza', 'image': 'https://spoonacular.com/recipeImages/644953-312x231.jpg', 'num_used_ings': 2, 'num_missing_ings': 2, 'missing_ings': ['Pizza Shell', 'Goat Cheese'], 'used_ings': ['Pesto', 'Tomatoes']}, 'Cream Cheese With Sun Dried Tomatoes And Pesto Pastry': {'id': 640513, 'title': 'Cream Cheese With Sun Dried Tomatoes And Pesto Pastry', 'image': 'https://spoonacular.com/recipeImages/640513-312x231.jpg', 'num_used_ings': 2, 'num_missing_ings': 3, 'missing_ings': ['Block Of Cream Cheese', 'Regular Crescents From The Section Of The Grocery', 'Egg - Beat'], 'used_ings': ['Pesto', 'Sundried Tomatoes']}, 'Pesto Fresh Caprese Sandwich': {'id': 655822, 'title': 'Pesto Fresh Caprese Sandwich', 'image': 'https://spoonacular.com/recipeImages/655822-312x231.jpg', 'num_used_ings': 2, 'num_missing_ings': 4, 'missing_ings': ['Balsamic Vinegar', 'Ciabatta Roll', 'Basil Leaves', 'Mozzarella'], 'used_ings': ['Basil Pesto', 'Tomato']}}
+        
+        if not recipes:
+            # handle error
+            ...
+        # json_file_path = os.path.join(settings.BASE_DIR, 'meal_app/static', 'working_data.json')
+
+        # with open(json_file_path, 'r') as file:
+        #     recipes = json.load(file)
+
+        return render(request, 'results.html', context={
+            'results': recipes
+            })
+def recipe(request, recipe_id):
+    
+    # get recipe details
+    print(recipeInformation(recipe_id))
+    return render(request, 'view_recipe.html', {})
