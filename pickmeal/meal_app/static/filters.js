5fb6aed58866365000a0aa92912a4a9c5db35521
100yann <stoyan.d.kolev@gmail.com>
2023-11-17 11:04:19 +0200

    implement filter that displays recipes by rating in descending order

---
+++ pickmeal/meal_app/static/filters.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttonTopRated = document.getElementById('top-rated-btn')
+    buttonTopRated.onclick = ((event) => {
+        event.preventDefault()
+        displayTopRated()
+    })
+})
+
+function displayTopRated() {
+    const recipesContainer = document.getElementById('saved-recipes');
+    const recipes = Array.from(document.querySelectorAll('#recipe-card'));
+
+    recipes.sort((a, b) => {
+        const ratingA = parseFloat(a.querySelector('#rating')?.dataset.rating || 0);
+        const ratingB = parseFloat(b.querySelector('#rating')?.dataset.rating || 0);
+        return ratingB - ratingA; // Sort in descending order
+    });
+
+    recipes.forEach((recipe) => {
+        recipesContainer.appendChild(recipe);
+    });
+}
