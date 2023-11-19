314eb77100f3e529891e7f859ceb511f5d173246
100yann <stoyan.d.kolev@gmail.com>
2023-11-19 15:12:42 +0200

    add filters for vegetarian, dairy free and gluten free meals as well

--- pickmeal/meal_app/static/filters.js
+++ pickmeal/meal_app/static/filters.js
@@ -19,6 +19,27 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault()
         displayByTag('vegan')
     })
+
+    // show only vegetarian recipes
+    const buttonVegetarian = document.getElementById('vegetarian')
+    buttonVegetarian.onclick = ((event) => {
+        event.preventDefault()
+        displayByTag('vegetarian')
+    })
+
+    // show only dairy free recipes
+    const buttonDairyFree = document.getElementById('dairy-free')
+    buttonDairyFree.onclick = ((event) => {
+        event.preventDefault()
+        displayByTag('dairyfree')
+    })
+
+    // show only gluten free recipes
+    const buttonGlutenFree = document.getElementById('gluten-free')
+    buttonGlutenFree.onclick = ((event) => {
+        event.preventDefault()
+        displayByTag('glutenfree')
+    })
 })
 
 function displayTopRated() {
@@ -66,14 +87,15 @@ function displayRecentlyAdded() {
 
 
 function displayByTag(tag){
-    const recipesContainer = document.getElementById('saved-recipes');
     const recipes = Array.from(document.querySelectorAll('#recipe-card'));
     recipes.forEach((recipe) => {
         const recipeTag = recipe.querySelector(`#is-${tag}`)
         const recipeTagValue = recipeTag.dataset[tag]
 
-        if (!recipeTagValue){
+        if (recipeTagValue === 'False' || !recipeTagValue){
             recipe.hidden = true;
+        } else if (recipeTagValue && recipe.hidden === true){
+            recipe.hidden = false;
         }
     })
 }
